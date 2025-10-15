b975a401225805a6853d289878454c2f37dd4312
maaziqbal2002 <maaziqbal@nextbridgesystem.com>
2025-10-16 01:17:53 +0500

    Installation of React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+    return (
+    <>
+      <div>
+        <h1 className='text-red-500 justify-center flex'>This is the setup of the project</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
