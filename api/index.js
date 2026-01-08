5a233d2e6df9ed515fe3bb023cfcf2afff3c2902
maaziqbal2002 <maaziqbal@nextbridgesystem.com>
2026-01-08 18:57:25 +0500

    refactor: enhance error handling in signup and server setup

--- api/index.js
+++ api/index.js
@@ -1,22 +1,34 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-}
-);
-app.use('/api/users', userRouter);
-app.use('/api/auth', authRouter);
+  console.log("Server is running on port 3000");
+});
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
